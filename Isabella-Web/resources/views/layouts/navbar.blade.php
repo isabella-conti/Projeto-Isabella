964f076128541f567297c90d02977f68ab46e726
isabella-conti <ircontif@gmail.com>
2024-03-14 02:55:42 -0300

    navbar bonita

--- Isabella-Web/resources/views/layouts/navbar.blade.php
+++ Isabella-Web/resources/views/layouts/navbar.blade.php
@@ -1,21 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar</title>
+    <title>Menu de Navegação Neumorfismo</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        .neumorphism {
+            background: #200e2c;
+            color: #f0f0f0;
+            border-radius: 10px;
+            padding: 10px 20px;
+            transition: background-color 0.3s ease, transform 0.3s ease;
+            box-shadow: 3px 3px 16px rgba(0, 0, 0, 0.5), -3px -3px 16px rgba(255, 255, 255, 0.1);
+            border: none;
+        }
+
+        .neumorphism:hover {
+            background: rgb(27, 1, 36);
+            box-shadow: 6px 6px 16px rgba(0, 0, 0, 0.5), -6px -6px 16px rgba(255, 255, 255, 0.1);
+            color: #fff;
+            transform: translateY(-2px) scale(0.98);
+        }
+
+
+        .bg-navbar {
+            background-color: #311843;
+        }
+    </style>
 </head>
+
 <body class="bg-gray-100">
-    <nav class="bg-gray-800 p-4 fixed top-0 w-full z-10 shadow-lg">
-        <div class="container mx-auto flex justify-end">
-            <a href="#home" class="text-purple-400 hover:text-white mr-4">Sobre Mim</a>
-            <a href="#courses" class="text-purple-400 hover:text-white mr-4">Cursos</a>
+    <nav class="bg-navbar p-4 fixed top-0 w-full z-10">
+        <div class="container mx-auto flex justify-end items-center">
+            <a href="#home" class="neumorphism text-gray-800 hover:text-white mr-4">Sobre mim</a>
+            <a href="#courses" class="neumorphism text-gray-800 hover:text-white mr-4">Cursos</a>
+            <a href="#contact" class="neumorphism text-gray-800 hover:text-white">Contato</a>
         </div>
     </nav>
 
 
     @yield('content')
-
 </body>
+
 </html>
